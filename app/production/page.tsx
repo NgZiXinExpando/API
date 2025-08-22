614e17a3d65b021e57675fbaeb5a71223e5caa74
NgZiXinExpando <zixin@expando.world>
2025-08-22 12:04:37 +0800

    test

---
+++ app/production/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div>
+      <iframe
+        src="https://v1.api.expando.ai/referral/iframe?key=sk_ccae23b3ebd8.e2a9d49598779b477815e53f3f2aaf0e&programId=8da35756-10a8-436a-b60d-b61774065ae4&programName=Expando Beta Test "
+        width="100%"
+        height="600"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default page;
